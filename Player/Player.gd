3bf3da1c99133c7c0540a4aa678d866b93c6c351
MichaelHofbeck <hofbeckm@bc.edu>
2022-11-12 16:27:10 -0500

    Update Player.gd

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,9 +8,9 @@ func _physics_process(delta):
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -4
 	elif Input.is_action_pressed("ui_up"):
-		velocity.y = 4
-	elif Input.is_action_pressed("ui_down"):
 		velocity.y = -4
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y = 4
 	else:
 		velocity.x = 0
 		velocity.y = 0
