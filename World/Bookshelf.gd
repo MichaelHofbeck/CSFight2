913fd254b684b71a54c03efd0889e884b98fc0b3
MichaelHofbeck <hofbeckm@bc.edu>
2022-11-13 04:47:21 -0500

    enemy dies

--- World/Bookshelf.gd
+++ World/Bookshelf.gd
@@ -1,14 +1,12 @@
 extends StaticBody2D
 
+var Effect = preload("res://Effects/BookshelfEffect.tscn")
 
-
-func create_bookshelf_effect():
-	var BookshelfEffect = load("res://Effects/BookshelfEffect.tscn")
-	var bookshelfEffect = BookshelfEffect.instance()
-	var world = get_tree().current_scene
-	world.add_child(bookshelfEffect)
-	bookshelfEffect.global_position = global_position
+func create_effect():
+	var effect = Effect.instance()
+	get_parent().add_child(effect)
+	effect.global_position = global_position
 	
 func _on_Hurtbox_area_entered(area):
-	create_bookshelf_effect()
+	create_effect()
 	queue_free()
