3c0dc6309ca97c427b765502200adb25c5d08509
eibrady <108247904+eibrady@users.noreply.github.com>
2022-11-12 22:11:09 -0500

    Title_screen_stuff

--- Title.gd
+++ Title.gd
@@ -10,7 +10,11 @@ extends Node2D
 func _ready():
 	yield(get_tree().create_timer(1), "timeout")
 	$AnimationPlayer.play("Fade")
-	
+	for button in $Menu/CenterRow/Buttons.get_children():
+		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
+		
+func _on_Button_pressed(scene_to_load):
+	get_tree().change_scene(scene_to_load)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
